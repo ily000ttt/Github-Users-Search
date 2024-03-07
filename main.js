a842b7da5b0d7eb1eda8feb4e91a79ee291d393c
ily000ttt <146103468+ily000ttt@users.noreply.github.com>
2024-03-07 18:45:47 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,24 @@ class View{
         this.searchInput.placeholder = "Write user name..."
         this.searchForm.append(this.searchInput)
 
+        this.main = this.createElement('div', 'main');
+
+        this.users = this.createElement('div', 'users')
+        this.usersList = this.createElement('div', 'users-list')
+        this.userLoadMoreBtn = this.createElement('button', 'button')
+        this.userLoadMoreBtn.textContent = "Load More"
+        this.users.append(this.usersList)
+        this.users.append(this.userLoadMoreBtn)
+
+        this.usersInfo = this.createElement("div", 'user-info')
+
+        this.main.append(this.users)
+        this.main.append(this.usersInfo)
+
+
         this.app.append(this.title)
         this.app.append(this.searchForm)
+        this.app.append(this.main)
     }
     createElement(elTag, elClass){
         const element = document.createElement(elTag)
@@ -21,10 +37,21 @@ class View{
         return element;
     }
 }
+class Search {
+    constructor(view){
+        this.view = view;
+        this.view.searchInput.addEventListener('keyup', this.searchUsers.bind(this))
+    }
+    async searchUsers(){
+        const inpValue = this.view.searchInput.value;
+        return await fetch(`https://api.github.com/search/users?q=${inpValue}`.then)
+    }
+}
 class App{
     constructor(view){
         this.view = view;
     }
 }
-
-const app = new App(new View())
+const view = new View()
+const search = new Search(view)
+const app = new App(view,search)
